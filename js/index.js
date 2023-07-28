d9d8f5934cb24702976be53539b1dd56ca22c7f5
matheus369k <gomes.craft10@gmail.com>
2023-07-28 18:26:11 -0300

    js... parte do calculo terminado...

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
 const conta = document.querySelector('#ibill');
 const people = document.getElementById('ipeople');
-const porcentos = document.querySelectorAll('.buttom');
 
-
-function amount(){
+function tip(){
+    const porcentos = document.querySelectorAll('.buttom');
     porcentos.forEach(porcento =>
         porcento.addEventListener('click', () => {
 
@@ -12,19 +11,46 @@ function amount(){
                 verif.classList.replace('on','off')
             }
             
-            porcento.classList.replace('off','on')
-
-            const porcentoId = Number(porcento.attributes.id.value)
-            const contaValue = Number(conta.value)
-            const peopleValue = Number(people.value)
-
-            let calc = ((contaValue*porcentoId)/100)/peopleValue
-
-            const msgAmount = document.getElementById('amount');
-            msgAmount.innerHTML = `${calc.toFixed(2)}`
-
-            console.log(calc);
+            porcento.classList.replace('off','on') 
             
         })
     )
+}
+
+function amount(){
+    let active = document.querySelector('.on');
+    let contaValue = Number(conta.value)
+    let peopleValue = Number(people.value)
+
+    const msgAmount = document.getElementById('amount');
+
+    if (peopleValue != '' && contaValue != '' && active != null){
+
+        let porcentoId = Number(active.attributes.id.value)
+
+        let calc = ((contaValue*porcentoId)/100)/peopleValue
+
+        msgAmount.innerHTML = `${calc.toFixed(2)}`
+    
+        return calc
+    
+        
+    }
+
+}
+
+function total(){
+    const msgTotal = document.querySelector('#total');
+    let contaValue = Number(conta.value)
+    let peopleValue = Number(people.value)
+
+    console.log(amount());
+
+    if (amount() != undefined){
+        let amountValue = Number(amount())
+        
+        let calc = (contaValue/peopleValue) + amountValue
+
+        msgTotal.innerHTML = `${calc.toFixed(2)}`}
+
 }
